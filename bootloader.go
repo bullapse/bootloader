885e69f9214af9cda1a7780b29f4a5c2da25cb5d
Spencer Bull <spencerbull2554@gmail.com>
2016-08-28 19:24:45 -0500

    create a new function RunWithLog to have the ability to pass in a log

--- bootloader.go
+++ bootloader.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"bufio"
 	"fmt"
+	"log"
 )
 
 func Run(fp string) {
@@ -16,5 +17,18 @@ func Run(fp string) {
 	for sc.Scan() {
 		l := sc.Text()
 		fmt.Println(l)
+	}
+}
+
+func RunWithLog(fp string, l log.Logger) {
+	f, e := os.Open(fp)
+	if e != nil {
+		l.Println("File at " + string(fp) + "cannot be found.")
+		l.Println(e)
+	}
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		li := sc.Text()
+		l.Println(li)
 	}
 }
